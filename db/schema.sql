461c360debccee9a22838673497044ae0976586f
Matthew Bull <mattbull33@gmail.com>
2023-06-19 10:18:15 -0400

    fixed schema?

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,8 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL, 
+    salary DECIMAL NOT NULL,
+    department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
@@ -21,10 +22,10 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
+    role_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
     ON DELETE SET NULL,
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
+    manager_id INT REFERENCES employee(id)
     ON DELETE SET NULL
 );
