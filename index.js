18948ae51e3bf131760fbf40d597f0300e28ecb7
Matthew Bull <mattbull33@gmail.com>
2023-06-20 11:23:02 -0400

    add Employee

--- index.js
+++ index.js
@@ -14,76 +14,82 @@ const connection = mysql.createConnection({
 });
 
 //start program
-function startPrompt(){
+function startPrompt() {
     prompt([
         {
             type: "list",
             name: "choice",
             message: "What would you like to do?",
-            choices: 
+            choices:
                 ["View All Departments",
-                "View All Roles",
-                "View All Employees",
-                "Add Department",
-                "Remove Department",
-                "Add Role",
-                "Remove Role",
-                "Add Employee",
-                "Remove Employee",
-                "Exit"
+                    "View All Roles",
+                    "View All Employees",
+                    "Add Department",
+                    "Remove Department",
+                    "Add Role",
+                    "Remove Role",
+                    "Add Employee",
+                    "Remove Employee",
+                    "Exit"
                 ]
-            }
-        ]).then((data) => {
-            switch (data.choice) {
-                case "View All Departments":
+        }
+    ]).then((data) => {
+        switch (data.choice) {
+            case "View All Departments":
                 viewAllDepartments();
                 break;
-                case "View All Roles":
+            case "View All Roles":
                 viewAllRoles();
                 break;
-                case "View All Employees":
+            case "View All Employees":
                 viewAllEmployees();
                 break;
-                case "Add Department":
+            case "Add Department":
                 addDepartment();
                 break;
-                case "Remove Department":
+            case "Remove Department":
                 removeDepartment();
                 break;
-                case "Add Role":
+            case "Add Role":
                 addRole();
                 break;
-                case "Remove Role":
+            case "Remove Role":
                 removeRole();
                 break;
-                case "Exit":
+            case "Add Employee":
+                addEmployee();
+                break;
+            case "Remove Employee":
+                removeEmployee();
+                break;
+            case "Exit":
                 exit();
                 break;
-            }
-        })
+        }
+    })
 }
 
-function startFollowup(){
+function startFollowup() {
     prompt([
         {
             type: "list",
             name: "choice",
             message: "What would you like to do next?",
-            choices: 
+            choices:
                 ["Go Back to Start",
-                "Exit",
+                    "Exit",
                 ]
-            }
-        ]).then((data) => {
-            switch (data.choice) {
-                case "Go Back to Start":
+        }
+    ]).then((data) => {
+        switch (data.choice) {
+            case "Go Back to Start":
                 startPrompt();
                 break;
-                case "Exit":
+            case "Exit":
                 exit();
                 break;
-            }
-        })
+        }
+    })
 }
 
 //return the table of departments 
@@ -124,23 +130,23 @@ function addDepartment() {
             name: "name",
             message: "What is the name of the Department",
         }
-        ]).then((data) => {
-            const newDepartment = (data.name)
-            const sql = "INSERT INTO department (name) VALUES (?)"
+    ]).then((data) => {
+        const newDepartment = (data.name)
+        const sql = "INSERT INTO department (name) VALUES (?)"
 
-            connection.promise().query(sql, newDepartment);
+        connection.promise().query(sql, newDepartment);
 
-            console.log(`${newDepartment} added to the Database`);
+        console.log(`${newDepartment} added to the Database`);
 
-            startFollowup();
+        startFollowup();
 
-        })
+    })
 }
 
 //remove Department
 function removeDepartment() {
     const data = connection.promise().query("SELECT * FROM department")
-    data.then(([data]) =>{
+    data.then(([data]) => {
         console.table(data);
     })
     prompt([
@@ -149,23 +155,55 @@ function removeDepartment() {
             name: "remove",
             message: "Enter the ID of the Department you would like to Remove",
         }
-        ]).then((data) => {
-            const deleteDepartment = (data.remove)
-            const sql = "DELETE FROM department WHERE id = (?)"
+    ]).then((data) => {
+        const deleteDepartment = (data.remove)
+        const sql = "DELETE FROM department WHERE id = (?)"
 
-            connection.promise().query(sql, deleteDepartment);
+        connection.promise().query(sql, deleteDepartment);
 
-            console.log(`Department removed from the Database`);
+        console.log(`Department removed from the Database`);
 
-            startFollowup();
+        startFollowup();
 
+    })
+}
+
+//Add Role
+const addRole = async () => {
+    var table = connection.promise().query("SELECT * FROM department")
+    table.then(([table]) => {
+        console.table(table);
+    })
+    await prompt([
+        {
+            type: "input",
+            name: "departmentId",
+            message: "Enter the ID of the department this role belongs to",
+        },
+        {
+            type: "input",
+            name: "title",
+            message: "What is the Title of the Role"
+        },
+        {
+            type: "input",
+            name: "salary",
+            message: "What is the Salary of the Role"
+        },
+    ]).then((data) => {
+        const sql = "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)";
+        const newRole = [data.title, data.salary, data.departmentId]
+        connection.promise().query(sql, newRole);
+
+        console.log(`${data.title} added to the Database`)
+        startFollowup();
     })
 }
 
 //Remove Role
 const removeRole = async () => {
     const data = connection.promise().query("SELECT * FROM role")
-    data.then(([data]) =>{
+    data.then(([data]) => {
         console.table(data);
     })
     prompt([
@@ -174,48 +212,61 @@ const removeRole = async () => {
             name: "remove",
             message: "Enter the ID of the Role you would like to Remove",
         }
-        ]).then((data) => {
-            const deleteRole = (data.remove)
-            const sql = "DELETE FROM role WHERE id = (?)"
+    ]).then((data) => {
+        const deleteRole = (data.remove)
+        const sql = "DELETE FROM role WHERE id = (?)"
 
-            connection.promise().query(sql, deleteRole);
+        connection.promise().query(sql, deleteRole);
 
-            console.log(`Role removed from the Database`);
+        console.log(`Role removed from the Database`);
 
-            startFollowup();
+        startFollowup();
 
     })
 }
 
-//Add Role
-const addRole = async () =>{
-    var table = connection.promise().query("SELECT * FROM department")
-    table.then(([table]) =>{
-    console.table(table);
+//Add Employee
+const addEmployee = async () => {
+    let table = connection.promise().query("SELECT id, title FROM role")
+    table.then(([table]) => {
+        console.table(table);
     })
     await prompt([
         {
-            type:"input",
-            name:"departmentId",
-            message: "Enter the ID of the department this role belongs to",
+            type: "input",
+            name: "roleId",
+            message: "Enter the ID of the role this employee belongs to",
         },
         {
-            type:"input",
-            name:"title",
-            message: "What is the Title of the Role"
+            type: "input",
+            name: "firstName",
+            message: "What is the First Name of the Employee"
         },
         {
-            type:"input",
-            name:"salary",
-            message: "What is the Salary of the Role"
+            type: "input",
+            name: "lastName",
+            message: "What is the Last Name of the Employee"
         },
-    ]).then((data) =>{
-        const sql = "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)";
-        const newRole = [data.title, data.salary, data.departmentId]
-        connection.promise().query(sql, newRole);
+    ]).then((data) => {
+        const newEmployee = [data.firstName, data.lastName, data.roleId]
+        let table = connection.promise().query("SELECT id, first_name, last_name FROM employee")
+        table.then(([table]) => {
+            console.table(table);
+        })
+        prompt([
+            {
+                type: "input",
+                name: "manager",
+                message: "Enter the ID of this Employees Manager (Enter for No Manager)",
+            }
+        ]).then((data) => {
+            newEmployee.push(data.manager)
+            const sql = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)"
+            connection.promise().query(sql, newEmployee)
 
-        console.log(`${data.title} added to the Database`)
-        startFollowup();
+            console.log(`${newEmployee[0]} + ${newEmployee[1]} added to database`)
+            startFollowup();
+        })
     })
 }
 
